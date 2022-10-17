2f878337aebfb9d62144cf8762aeea671abf7a7f
sczJAIA <j.alejandro460@gmail.com>
2022-10-17 16:00:49 -0400

    main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
 
   // Configurar títulos de documentación
   const options = new DocumentBuilder()
@@ -13,6 +15,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
+  process.on('uncaughtException', (err) => {
+    Logger.warn(err, 'LOGGER', false);
+  });
 
   // La ruta en que se sirve la documentació
   SwaggerModule.setup('docs', app, document);
