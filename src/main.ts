849889741a07161e22724b7c64545d6fd983639f
sczJAIA <j.alejandro460@gmail.com>
2022-10-17 14:04:40 -0400

    ports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
@@ -19,6 +17,6 @@ async function bootstrap() {
   // La ruta en que se sirve la documentación
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
