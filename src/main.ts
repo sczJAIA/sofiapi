819d42c734cb43634fb95e57f329206fe7e43e25
sczJAIA <j.alejandro460@gmail.com>
2022-10-17 15:16:21 -0400

    dist

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   // La ruta en que se sirve la documentación
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
