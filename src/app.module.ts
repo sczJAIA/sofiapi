ddf7c6f6d044fa668ee201cb3873611581650627
sczJAIA <j.alejandro460@gmail.com>
2022-10-18 11:25:13 -0400

    secret key

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      // process.env.URIMONGO,
-      'mongodb+srv://alejandrodev:2L8MrmvWP635U409@db-mongodb-nyc1-33154-559f29c0.mongo.ondigitalocean.com/?authMechanism=DEFAULT',
-    ),
+    MongooseModule.forRoot(process.env.URIMONGOPROD),
     AdvisorsModule,
     AuthModule,
   ],
